a1f8627180d3ad907902d2764657d19787af9ca6
codernishchay <nishchay.vns2000@gmaiil.com>
2021-08-16 06:51:23 -0400

    react_first_spp

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0); 
+   return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={()=> setCount(count+1)}> 
+      click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
